02ae3a475f47cf67be0ff92e36e86aeca3001cea
Gustavo Henrique Azambuja <guazambujinha@gmail.com>
2025-03-12 00:19:29 +0000

    teste

--- aula03/src/Projeto1A/App.java
+++ aula03/src/Projeto1A/App.java
@@ -5,5 +5,6 @@ public class App {
         System.out.println("\nHello");
         String  version = System.getProperty("java.version");
         System.out.println("Running Java Version " + version);
+        System.out.println("Testanto mudança");
     }
 }
